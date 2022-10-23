433ada1ba1a1984c9a1bea7894a3fb81336542b1
Brad <broswen@gmail.com>
2022-10-23 11:54:19 -0500

    poller poc

--- cmd/poller/main.go
+++ cmd/poller/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"context"
+	"github.com/broswen/notifi/internal/db"
 	"github.com/broswen/notifi/internal/queue/producer"
+	"github.com/broswen/notifi/internal/repository"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 
-	// parse queue topic
-	// parse brokers
-	// parse dsn
-	// parse poll interval
 	brokers := os.Getenv("BROKERS")
 	if brokers == "" {
 		log.Fatal().Msgf("kafka brokers are empty")
@@ -40,20 +41,62 @@ func main() {
 		log.Fatal().Err(err).Msg("error creating kafka producer")
 	}
 
+	pool, err := db.InitDB(context.Background(), dsn)
+	if err != nil {
+		log.Fatal().Err(err).Msg("error creating postgres pool")
+	}
+	scheduledRepo, err := repository.NewScheduledNotificationSqlRepository(pool)
+	if err != nil {
+		log.Fatal().Err(err).Msg("error creating scheduled notification repository")
+	}
+	eg := errgroup.Group{}
+
 	ticker := time.NewTicker(interval)
 	ctx, cancel := context.WithCancel(context.Background())
 
-	for {
-		select {
-		case <-ticker.C:
-			log.Debug().Msg("polling for scheduled messages")
-		case <-ctx.Done():
-
+	//notifications can be delivered up to 5 minutes early
+	pollPeriod := time.Minute * 5
+	pollLimit := int64(100)
+	eg.Go(func() error {
+		for {
+			select {
+			case <-ticker.C:
+				log.Debug().Str("interval", interval.String()).Int64("limit", pollLimit).Msg("polling for scheduled messages")
+				notifications, err := scheduledRepo.ListScheduled(ctx, pollPeriod, pollLimit)
+				if err != nil {
+					log.Error().Err(err).Msg("error listing scheduled notifications")
+					continue
+				}
+				for _, n := range notifications {
+					//TODO mark in-progress to avoid resubmitting during another poll
+					log.Debug().Str("notification_id", n.ID).Time("schedule", *n.Schedule).Msg("submitting scheduled notification")
+					err = p.Submit(n)
+					if err != nil {
+						log.Error().Err(err).Str("notification_id", n.ID).Msg("error submitting scheduled notification")
+						continue
+					}
+				}
+			case <-ctx.Done():
+				log.Debug().Msg("context cancelled")
+				return nil
+			}
 		}
+	})
+
+	eg.Go(func() error {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+		s := <-sigs
+		log.Debug().Str("signal", s.String()).Msg("received signal")
+		cancel()
+		if err := p.Close(); err != nil {
+			log.Error().Err(err).Msg("error closing kafka producer")
+			return err
+		}
+		return nil
+	})
+
+	if err = eg.Wait(); err != nil {
+		log.Error().Err(err).Msg("")
 	}
-	// scan db for notifications where the scheduled time is less than X minutes in the future
-	// submit to queue
-	// remove from db (or mark sent for a cleanup job)
-	// store successful notifications in postgres
-	//		store failed notification status
 }
---
+++ internal/repository/scheduled_notification.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"context"
+	"github.com/broswen/notifi/internal/db"
+	"github.com/broswen/notifi/internal/entity"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"time"
+)
+
+type ScheduledNotificationRepository interface {
+	ListScheduled(ctx context.Context, period time.Duration, limit int64) ([]entity.Notification, error)
+}
+
+type ScheduledNotificationSqlRepository struct {
+	pool *pgxpool.Pool
+}
+
+func NewScheduledNotificationSqlRepository(pool *pgxpool.Pool) (ScheduledNotificationRepository, error) {
+	return ScheduledNotificationSqlRepository{
+		pool: pool,
+	}, nil
+}
+
+func (r ScheduledNotificationSqlRepository) ListScheduled(ctx context.Context, period time.Duration, limit int64) ([]entity.Notification, error) {
+	//p is the maximum time we are willing to send notifications early
+	p := time.Now().Add(period)
+	//TODO verify proper filtering order with compound index
+	rows, err := r.pool.Query(ctx, `select id, email_destination, sms_destination, content, schedule, deleted_at, created_at, modified_at, delivered_at from notification 
+		where delivered_at is null
+		and deleted_at is null 
+		and schedule is not null
+		and schedule < $1
+		order by schedule asc
+		limit $2;`, p, limit)
+	err = db.PgError(err)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	notifications := make([]entity.Notification, 0)
+	for rows.Next() {
+		n := entity.Notification{}
+		err = rows.Scan(&n.ID, &n.Destination.Email, &n.Destination.SMS, &n.Content, &n.Schedule, &n.DeletedAt, &n.CreatedAt, &n.ModifiedAt, &n.DeliveredAt)
+		if err != nil {
+			return notifications, err
+		}
+		notifications = append(notifications, n)
+	}
+	if rows.Err() != nil {
+		return notifications, err
+	}
+	return notifications, err
+}
